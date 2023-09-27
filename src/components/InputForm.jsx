7323f65c6b627d39d4b7610c4568d427c3b2eb98
shinohara <taisei.shinohara1329@gmail.com>
2023-09-28 08:24:11 +0900

    feat: Title,InputFormのComponentsを作成

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const InputForm = () => {
+  return (
+    <div className='inputForm'>
+      <form>
+        <input type='text'></input>
+        <button>
+        <i class="fa-solid fa-plus"></i>
+        </button>
+      </form>
+    </div>
+  )
+}
